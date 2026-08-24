3a0d6fff77b4de75318e483be60c6a587807c60e
NatBypass Author <dev@natbypass.internal>
2026-08-24 21:18:16 +0300

    fix(security): remove runtime runas self-relaunch to eliminate Windows Defender ML false positive; embed requireAdministrator in PE manifest

--- cmd/natbypass/elevate_windows.go
+++ cmd/natbypass/elevate_windows.go
@@ -3,43 +3,12 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
-	"unsafe"
-
 	"github.com/natbypass/natbypass/internal/diagnostic"
-	"golang.org/x/sys/windows"
+	"github.com/rs/zerolog/log"
 )
 
 func ensureAdminOnWindows() {
 	if !diagnostic.CheckIsAdmin() {
-		verb, _ := windows.UTF16PtrFromString("runas")
-		exePath, err := os.Executable()
-		if err != nil {
-			return
-		}
-		exePathPtr, _ := windows.UTF16PtrFromString(exePath)
-		dirPtr, _ := windows.UTF16PtrFromString(filepath.Dir(exePath))
-
-		var args string
-		if len(os.Args) > 1 {
-			args = strings.Join(os.Args[1:], " ")
-		}
-		argsPtr, _ := windows.UTF16PtrFromString(args)
-
-		modshell32 := windows.NewLazySystemDLL("shell32.dll")
-		procShellExecuteW := modshell32.NewProc("ShellExecuteW")
-		ret, _, _ := procShellExecuteW.Call(
-			0,
-			uintptr(unsafe.Pointer(verb)),
-			uintptr(unsafe.Pointer(exePathPtr)),
-			uintptr(unsafe.Pointer(argsPtr)),
-			uintptr(unsafe.Pointer(dirPtr)),
-			1, /* SW_SHOWNORMAL */
-		)
-		if ret > 32 {
-			os.Exit(0)
-		}
+		log.Warn().Msg("⚠️ Внимание: Приложение запущено без прав администратора. Для создания виртуального интерфейса Wintun и настройки сетевых маршрутов рекомендуем запуск от имени Администратора.")
 	}
 }
