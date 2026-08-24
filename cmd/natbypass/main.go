e39880074415c23e40a7364c86a7f9134e3f06b8
NatBypass Author <dev@natbypass.internal>
2026-08-24 17:07:33 +0300

    feat(windows): add automatic UAC self-elevation on double click in Explorer

--- cmd/natbypass/main.go
+++ cmd/natbypass/main.go
@@ -14,10 +14,12 @@ import (
 	"strings"
 	"syscall"
 	"time"
+	"unsafe"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/windows"
 
 	"github.com/natbypass/natbypass/internal/config"
 	"github.com/natbypass/natbypass/internal/crypto"
@@ -61,6 +63,9 @@ var (
 )
 
 func main() {
+	// Автоматический запрос прав Администратора через UAC на Windows при обычном запуске
+	ensureAdminOnWindows()
+
 	// Если процесс запущен под управлением Windows Service Manager
 	if daemon.IsWindowsService() {
 		err := daemon.RunService(func(ctx context.Context) error {
@@ -1278,6 +1283,33 @@ func newDiagCmd() *cobra.Command {
 }
 
 // ── helpers ────────────────────────────────────────────────────
+
+func ensureAdminOnWindows() {
+	if runtime.GOOS != "windows" {
+		return
+	}
+	if !diagnostic.CheckIsAdmin() {
+		verb, _ := windows.UTF16PtrFromString("runas")
+		exePath, err := os.Executable()
+		if err != nil {
+			return
+		}
+		exePathPtr, _ := windows.UTF16PtrFromString(exePath)
+
+		var args string
+		if len(os.Args) > 1 {
+			args = strings.Join(os.Args[1:], " ")
+		}
+		argsPtr, _ := windows.UTF16PtrFromString(args)
+
+		modshell32 := windows.NewLazySystemDLL("shell32.dll")
+		procShellExecuteW := modshell32.NewProc("ShellExecuteW")
+		ret, _, _ := procShellExecuteW.Call(0, uintptr(unsafe.Pointer(verb)), uintptr(unsafe.Pointer(exePathPtr)), uintptr(unsafe.Pointer(argsPtr)), 0, 1 /* SW_SHOWNORMAL */)
+		if ret > 32 {
+			os.Exit(0)
+		}
+	}
+}
 
 func setupLogging(level, logFile string) {
 	zerolog.TimeFieldFormat = time.RFC3339
