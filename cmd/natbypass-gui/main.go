bbfbef41fb1ee3412ef27d600c334973301e2702
NatBypass Author <dev@natbypass.internal>
2026-08-31 16:11:59 +0300

    fix(tunnel): use native netsh static binding for Windows Wintun and sync v1.9.147

--- cmd/natbypass-gui/main.go
+++ cmd/natbypass-gui/main.go
@@ -95,7 +95,7 @@ func applyAWGProfileToGUI(p *config.Profile) {
 
 
 var (
-	Version = "1.9.146"
+	Version = "1.9.147"
 	Commit  = "release"
 )
 
--- cmd/natbypass/main.go
+++ cmd/natbypass/main.go
@@ -15,7 +15,7 @@ import (
 
 
 var (
-	Version   = "1.9.146"
+	Version   = "1.9.147"
 	Commit    = "unknown"
 	BuildDate = "unknown"
 )
