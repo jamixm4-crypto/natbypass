1600f8dafa5a28650bd018c5150204116718e2c2
NatBypass Author <dev@natbypass.internal>
2026-08-31 22:42:23 +0300

    fix(tunnel): clean stale routes and IPs on other interfaces during Wintun setup (v1.9.167)

--- cmd/natbypass-gui/main.go
+++ cmd/natbypass-gui/main.go
@@ -95,7 +95,7 @@ func applyAWGProfileToGUI(p *config.Profile) {
 
 
 var (
-	Version = "1.9.166"
+	Version = "1.9.167"
 	Commit  = "release"
 )
 
--- cmd/natbypass/main.go
+++ cmd/natbypass/main.go
@@ -15,7 +15,7 @@ import (
 
 
 var (
-	Version   = "1.9.166"
+	Version   = "1.9.167"
 	Commit    = "unknown"
 	BuildDate = "unknown"
 )
