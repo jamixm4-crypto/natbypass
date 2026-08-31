95d052a2a6e8ab43d334d4246ee9cea2500b6036
NatBypass Author <dev@natbypass.internal>
2026-08-31 22:34:04 +0300

    fix(tunnel): check existing IP before assigning to avoid removing active Wintun address (v1.9.165)

--- cmd/natbypass-gui/main.go
+++ cmd/natbypass-gui/main.go
@@ -95,7 +95,7 @@ func applyAWGProfileToGUI(p *config.Profile) {
 
 
 var (
-	Version = "1.9.164"
+	Version = "1.9.165"
 	Commit  = "release"
 )
 
--- cmd/natbypass/main.go
+++ cmd/natbypass/main.go
@@ -15,7 +15,7 @@ import (
 
 
 var (
-	Version   = "1.9.164"
+	Version   = "1.9.165"
 	Commit    = "unknown"
 	BuildDate = "unknown"
 )
